aaa045b7cf22c57da86ef2635aa694dc48a30f9d
nhat <S3878174@RMIT.EDU.VN>
2021-07-26 13:04:47 +0700

    Add NASDAQ stock exchange

--- Task3/twelvedata-producer/twelvedata_producer.py
+++ Task3/twelvedata-producer/twelvedata_producer.py
@@ -20,7 +20,7 @@ td = TDClient(apikey=API_KEY)
 
 #Change the symbol of the ticker here
 ts = td.time_series(
-    exchange ="NYSE",
+    exchange ="NASDAQ",
     symbol= TICKER,
     interval= INTERVAL,
     timezone="America/New_York",
